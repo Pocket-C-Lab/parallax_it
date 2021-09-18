aed98317e066b74fae1d8596a5bc09471b6c0fb5
Brain Developer <ssaulakh2@gmail.com>
2021-09-18 22:27:13 +0530

    clouds added

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,14 @@
+/*jshint esversion: 6 */
+
 var app = angular.module('app-main', ['ngRoute', 'ng-context-menu']);
 
 app.controller('side-nav', function displayMessage($scope, stringService) {
 	$scope.message = "Hello World!";
 	$scope.transformString = function (input) {
 		$scope.output = stringService.processString(input);
-	}
+	};
+
+	$scope.images = [];
 
 	var brush = {
 		name: "brush",
@@ -18,6 +22,16 @@ app.controller('side-nav', function displayMessage($scope, stringService) {
 		['J', 'K', 'L', 'M',
 		 'N', 'O', 'P', 'Q']
 	;
+	$scope.speed = {
+		value: 50,
+		options: {
+		  floor: 0,
+		  ceil: 100,
+		  step: 1,
+		  minLimit: 10,
+		  maxLimit: 90
+		}
+	};
 	$scope.selected_tool = 'K';
 	$scope.tool_options = {
 		J: ['line', 'pen', 'pencil'],
@@ -46,7 +60,7 @@ app.controller('side-nav', function displayMessage($scope, stringService) {
 	    }
 		$scope.selected_tool = s;
 		console.log($scope.selected_tool + " " + $scope.tool_options[$scope.selected_tool]);
-	}
+	};
 
 	// tools logic starts here...
 	var settings = 
@@ -64,6 +78,9 @@ app.controller('side-nav', function displayMessage($scope, stringService) {
 	$scope.brush = brush;
 	$scope.tools = tools;
 	$scope.settings = settings;
+	$scope.printImages = function() {
+		console.log($scope.images);
+	};
 	$scope.o_oO = function(i) {
 		switch(i) {
 			case 'C':
@@ -79,7 +96,7 @@ app.controller('side-nav', function displayMessage($scope, stringService) {
 				$scope.message = "Option 4";
 			break;
 		}
-	}
+	};
 	$scope.o_oF = function(i) {
 		switch(i) {
 			case 'C':
@@ -91,63 +108,53 @@ app.controller('side-nav', function displayMessage($scope, stringService) {
 			break;
 			case 'H':
 				$scope.message = "Import";
-				console.log(i);
 				var fileSelector = document.createElement('input');
 				fileSelector.setAttribute('type', 'file');
-				fileSelector.setAttribute('accept', 'image/gif, image/jpeg, image/png');
+				fileSelector.setAttribute('accept', 'image/webp, image/gif, image/jpeg, image/png');
 
 			    fileSelector.click();
 			    fileSelector.onchange = function(e) {
-				    // img = new Image();
+				    img = new Image();
 				    var file = e.target.files[0];
 
 				    var reader = new FileReader();
 			        reader.onloadend = function() {
-			             img.src = reader.result;
-			        }
-			        reader.readAsDataURL(file);
-
-
-			        var newWindow = window.open("", "_blank", "height=56,width=56,status=yes,toolbar=no,menubar=no,location=no");  
-			        
-			        newWindow.document.write("<canvas id='imagePlaceholder'>Canvas not supported!</canvas>");
-			        var canvas = newWindow.document.getElementById("imagePlaceholder");
-			        var ctx = canvas.getContext("2d");
-
-			        ctx.drawImage(img, 0, 0);
-			        // console.log(ctx.getImageData(0, 0, 56, 56).data);
-			        dest = ctx.getImageData(0, 0, img.width, img.height).data;
-			        console.log(dest);
-			        newWindow.close();
-			        draw = true;
-			    }
+						console.log(reader.result);
+						
+						img = $scope.s.createImg(reader.result, '');
+						img.hide();
+						$scope.images.push({"img": img, "name": file.name});
+						$scope.$apply();
+			        };
+			        img = reader.readAsDataURL(file);
+			    };
 			break;
 			case 'G':
 				$scope.message = "Export";
 			break;
 		}
-	}
+	};
 
 	$scope.toolsVisibility = "";
 	$scope.tools_visibility = () => {
 		if ($scope.toolsVisibility == "")
 			$scope.toolsVisibility = "none";
 		else $scope.toolsVisibility = "";
-	}
+	};
 
 	$scope.settingsVisibility = "";
 	$scope.settings_visibility = () => {
 		if ($scope.settingsVisibility == "")
 			$scope.settingsVisibility = "none";
 		else $scope.settingsVisibility = "";
-	}
+	};
 
 	$scope.dropdownVisibility = "";
 	$scope.dropdown_visibility = () => {
 		if ($scope.dropdownVisibility == "")
 			$scope.dropdownVisibility = "none";
 		else $scope.dropdownVisibility = "";
-	}
+	};
 
 	$scope.openContextMenu = (evt) => {
 		evt.preventDefault();
@@ -155,83 +162,87 @@ app.controller('side-nav', function displayMessage($scope, stringService) {
 		menu.hide();
 		setTimeout(() => { menu.show(evt.pageX, evt.pageY) }, time);
 		document.addEventListener('click', hideContextMenu, false);
-	}
+	};
 	$scope.hideContextMenu = (evt) => {
 		menu.hide();
 		document.removeEventListener('click', hideContextMenu);
-	}
+	};
 	$scope.inputChanged = () => {
 		console.log("Changed!");
-	}
+	};
+
 
 
 	var canvas = document.getElementById("canvas");
 	var redraw = true, width = window.innerWidth - 460, height = window.innerHeight - 75;
 	var canvasX1 = canvas.offsetLeft, canvasY1 = canvas.offsetTop, canvasX2 = canvasX1+width, canvasY2 = canvasY1+height;
-	var pixels = new Array(), pixelRow=0;
 	var colors = ['white', 'black'], colorSelected = 1;
+	let img;
+
+	let x = 0;
+	let x2 = 360;
+
+	class Layer {
+		constructor(image1, image2, speedModifier) {
+			this.x = 0;
+			this.y = 0;
+			this.width = 360;
+			this.height = 450;
+			this.x2 = this.width;
+			this.image1 = image1;
+			this.image2 = image2;
+			this.speedModifier = speedModifier;
+			this.speed = gameSpeed * this.speedModifier;
+		}
+		
+		update() {
+			this.speed = gameSpeed * this.speedModifier;
+			if (this.x <= -this.width) {
+				this.x = this.width;
+				// this.x = this.width + this.x - this.speed;
+			} if (this.x2 <= -this.width) {
+				this.x2 = this.width;
+				// this.x2 = this.width + this.x - this.speed;
+			}
+			this.x = Math.floor(this.x - this.speed);
+			this.x2 = Math.floor(this.x2 - this.speed);
+		}
+		draw() {
+			ctx.drawImage(this.image1, this.x, this.y, this.width, this.height);
+			ctx.drawImage(this.image2, this.x2, this.y, this.width, this.height);
+		}
+	}
 
 	let sketch = function(p) {
 		var x=0, y=0;
 		p.setup = () => {
-			p5.disableFriendlyErrors = true;
+			// p5.disableFriendlyErrors = true;
 			p.createCanvas(width, height);
 			p.background("#FFFFFF");	//#f5f5f6
 			p.strokeWeight(0.2);
-
-			pixels.push(new Array());
-			while (y < height) {
-				pixels[pixelRow].push(new PixelRec(p, x, y, settings[0].value, settings[4].value));
-				if (x > width) {
-					x=0;
-					y+=settings[0].value;
-					pixels.push(new Array());
-					pixelRow++;
-				} else {
-					x+=settings[0].value;
-				}
-			}
-			for (var i=0; i<pixels.length; i++) {
-				for (var j=0; j<pixels[i].length; j++) {
-					pixels[i][j].resize(settings[0].value);
-					pixels[i][j].draw();
-				}
-			}
-			redraw = false;
-		}
+			
+		};
 
 		p.draw = () => {
-			if (redraw) {
-				let a = (p.mouseX)/settings[0].value;
-				let b = (p.mouseY)/settings[0].value;
-				a = parseInt(a);
-				b = parseInt(b);
-				pixels[b][a].draw();
-				redraw = false;
+			if ($scope.images) {
+				$scope.images.forEach(element => {
+					p.image(element.img, 0, 0, element.img.width, element.img.height);					
+				});
 			}
-		}
-		p.mouseClicked = () => {
-			canvasClick();
-	  	}
+		};
+		p.mouseClicked = () => {};
 		p.mouseWheel = (event) => {
 			// print(event.delta);
 			if (event.delta > 0 || settings[0].value > 1)
 			settings[0].value += event.delta/2;
 			// resize();
 			//return false;
-		}
-		p.mouseMoved = () => {}
-		p.mouseDragged = (event) => {
-			// for (var i=0; i<pixels.length; i++) {
-			// 	for (var j=0; j<pixels[i].length; j++) {
-			// 		pixels[i][j].click();
-			// 	}
-			// }
-			canvasClick();
-		}
-		p.mousePressed = () => {}
-		p.mouseReleased = () => {}
-		p.doubleClicked = () => {}
+		};
+		p.mouseMoved = () => {};
+		p.mouseDragged = (event) => {};
+		p.mousePressed = () => {};
+		p.mouseReleased = () => {};
+		p.doubleClicked = () => {};
 		p.keyTyped = () => {
 			switch(p.key) {
 				case 'z':
@@ -242,65 +253,10 @@ app.controller('side-nav', function displayMessage($scope, stringService) {
 					break;
 			}
 			return false;
-		}
-		p.requestPointerLock = () => {}
-		p.exitPointerLock = () => {}
-
-		canvasClick = () => {
-			if (event.x > canvasX1 && event.y > canvasY1
-				 && event.x < canvasX2 && event.y < canvasY2) {
-					let a = (p.mouseX)/settings[0].value;
-					let b = (p.mouseY)/settings[0].value;
-					a = parseInt(a);
-					b = parseInt(b);
-					pixels[b][a].click(colors[colorSelected]);
-					redraw = true;
-				}
-		}
-		resize = () => {
-			// resize the canvas according to the zoom level
-
-			// draw the resized canvas
-			for (var i=0; i<pixels.length; i++) {
-				for (var j=0; j<pixels[i].length; j++) {
-					pixels[i][j].resize(settings[0].value);
-					pixels[i][j].draw();
-				}
-			}
-			redraw = false;
-		}
+		};
+		p.requestPointerLock = () => {};
+		p.exitPointerLock = () => {};
 	};
 
 	$scope.s = new p5(sketch, 'canvas');
 });
-
-// app.directive( "ngContextmenu", function(){
-//     contextMenu = {replace: false};
-//     contextMenu.restrict = "AE";
-    
-//     contextMenu.scope = {"visible": "="};
-//     contextMenu.link = function( $scope, lElem, lAttr ){
-//         lElem.on("contextmenu", function (e) {
-                
-//                 e.preventDefault();
-            
-//                 console.log("Element right clicked.");
-//                 $scope.$apply(function () {
-//                      $scope.visible = true;
-//                 })
-            
-                
-      
-//         });
-//         lElem.on("mouseleave", function(e){
-         
-//                 console.log("Leaved the div");
-//                  console.log("Element right clicked.");
-//                 $scope.$apply(function () {
-//                      $scope.visible = false;
-//                 })
-         
-//         });
-//     };
-//     return contextMenu;
-// });
